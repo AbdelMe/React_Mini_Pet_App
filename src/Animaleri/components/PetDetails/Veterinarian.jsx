729a34178e49b959fc48d730d450ca66eb3f1a78
AbdelMe <simoelabdellaoui08@gmail.com>
2025-01-28 00:20:17 +0000

    updating Database

--- src/Animaleri/components/PetDetails/Veterinarian.jsx
+++ src/Animaleri/components/PetDetails/Veterinarian.jsx
@@ -1,5 +1,7 @@
-import { useSelector } from 'react-redux';
-import Sidebar from '../../Home'
+import { useSelector } from "react-redux";
+import Sidebar from "../../Home";
+import phone from '../../Pictures/image-gallery.png'
+import email from '../../Pictures/email.png'
 
 export default function Veterinarian() {
   const data = useSelector((state) => state.Compt.DataBase.Veterinarians);
@@ -7,7 +9,7 @@ export default function Veterinarian() {
     <>
       <Sidebar>
         <h1>Available Veterinarians</h1>
-        <table border={1} className='container'>
+        <table border={1} className="container">
           <tr>
             <th>First Name</th>
             <th>Last Name</th>
@@ -16,20 +18,32 @@ export default function Veterinarian() {
             <th>Phone</th>
             <th>Action</th>
           </tr>
-            {data.map(vet=>{
-              return <>
-          <tr>
-                <td>{vet.f_name}</td>
-                <td>{vet.l_name}</td>
-                <td><img src={vet.pic} width={100} alt=""  className='rounded-4'/></td>
-                <td>{vet.email}</td>
-                <td>{vet.phone}</td>
-                <td><a href={`mailto:${vet.email}`}>Call</a> <a href={`mailto:${vet.phone}`}>Email</a></td>
-          </tr>
+          {data.map((vet) => {
+            return (
+              <>
+                <tr>
+                  <td>{vet.f_name}</td>
+                  <td>{vet.l_name}</td>
+                  <td>
+                    <img
+                      src={vet.pic}
+                      width={100}
+                      alt=""
+                      className="rounded-4"
+                    />
+                  </td>
+                  <td>{vet.email}</td>
+                  <td>{vet.phone}</td>
+                  <td>
+                    <a href={`mailto:${vet.email}`}><img src={phone} width={30} alt="" className="mx-1"/></a>
+                    <a href={`mailto:${vet.phone}`}><img src={email} width={30} alt="" className="mx-1"/></a>
+                  </td>
+                </tr>
               </>
-            })}
+            );
+          })}
         </table>
       </Sidebar>
     </>
-  )
+  );
 }
